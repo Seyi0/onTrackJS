a6e70ee0a83341402ea9ef104cca8845c5576b17
Seyi0 <makanjuolaseyi560@gmail.com>
2024-08-01 22:24:17 +0100

    :fix:

--- src/app.js
+++ src/app.js
@@ -1,4 +1,20 @@
 // import {v4 as uuidv4} from 'uuid';
-const {v4: uuid} = require('uuid');
+// const {v4: uuid} = require('uuid');
 
-console.log (uuidv4());
+// console.log (uuidv4());
+
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res)=>{
+    res.send('hey the new don is in town')
+})
+
+app.post("/", (req, res) => {
+    res.send("post request template");
+});
+
+app.listen(PORT, ()=>{
+    console.log ("App listening on port " + PORT)
+})
