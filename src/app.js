935599557a03b55410939cf57516a07bec3921a2
Seyi0 <makanjuolaseyi560@gmail.com>
2024-08-03 22:58:53 +0100

    :completed:

--- src/app.js
+++ src/app.js
@@ -5,16 +5,38 @@
 
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 const PORT = 3000;
 
+mongoose.set('strictQuery', false)
+
+const json = {
+    "name": "Oluwaseyi Makay",
+    "occupation":"student"
+};
+
 app.get('/', (req, res)=>{
-    res.send('hey the new don is in town')
+    res.send({"data":json})
 })
 
 app.post("/", (req, res) => {
     res.send("post request template");
 });
 
-app.listen(PORT, ()=>{
-    console.log ("App listening on port " + PORT)
-})
+
+
+const start = async () =>{
+    try{
+        await mongoose.connect(
+        "mongodb+srv://Seyi:R1GqNWxmUJZoXibZ@cluster-test.rpdaf04.mongodb.net/?retryWrites=true&w=majority&appName=Cluster-test"
+        );
+
+        app.listen(PORT, () => {
+        console.log("App listening on port " + PORT);
+        });
+    } catch(e){
+        console.log(e.message)
+    }
+};
+
+start();
